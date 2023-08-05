eae3c6cbe4a879eb6e0b1e66fd171a3cde5e7677
ameya-6964 <belvalkar7@gmail.com>
2023-08-05 19:20:49 +0530

    Added Create Account FXML With Structure

--- createaccount/CreateAccountController.java
+++ createaccount/CreateAccountController.java
@@ -8,16 +8,10 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author DELL
- */
+
 public class CreateAccountController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
