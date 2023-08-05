8feb1ff9cc281ab58bb4ff9ba2af7cb1d27cb2fc
ameya-6964 <belvalkar7@gmail.com>
2023-08-05 14:01:58 +0530

    Added Basic Folders For Images

--- login/Banking.java
+++ login/Banking.java
@@ -1,5 +1,5 @@
 
-package banking;
+package login;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- login/LoginScreenController.java
+++ login/LoginScreenController.java
@@ -1,6 +1,6 @@
 
 
-package banking;
+package login;
 
 import java.net.URL;
 import java.util.ResourceBundle;
