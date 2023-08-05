f4f415af54de70ffaaa1b4da9d80a8b6193dfd08
ameya-6964 <belvalkar7@gmail.com>
2023-08-05 22:24:40 +0530

    Minor Bug Fixes For Login And Creating Account

--- login/LoginScreenController.java
+++ login/LoginScreenController.java
@@ -2,18 +2,24 @@
 
 package login;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 
 
 public class LoginScreenController implements Initializable {
     
+    @FXML
+    private Pane main_area;
    
     
     @FXML
@@ -21,6 +27,14 @@ public class LoginScreenController implements Initializable {
         Platform.exit();
         System.exit(0);
         
+    }
+    
+     @FXML
+    private void createAccount(MouseEvent event) throws IOException {
+        
+        Parent fxml= FXMLLoader.load(getClass().getResource("/createaccount/CreateAccount.fxml"));
+         main_area.getChildren().removeAll();
+         main_area.getChildren().addAll(fxml);
     }
     
     @Override
